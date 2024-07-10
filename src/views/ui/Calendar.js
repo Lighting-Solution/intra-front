b11810ff1b7ae891b4961f939e06c24e2a79623b
minseong-Kang <80028813+minseong00@users.noreply.github.com>
2024-07-10 13:01:25 +0900

    병합수정

--- src/views/ui/Calendar.js
+++ src/views/ui/Calendar.js
@@ -1,411 +1,180 @@
-import React, { useState, useEffect } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import axios from "axios";
-import Modal from "react-modal";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import "./Calendar.css";
-import AddAttendeeModal from "./AddAttendeeModal.js";
-import { FaTimes } from "react-icons/fa";
-import moment from "moment-timezone";
+import React, { useEffect } from "react";
+import Calendar from "tui-calendar"; /* ES6 */
+import "tui-calendar/dist/tui-calendar.css";
 
-const localizer = momentLocalizer(moment);
+// If you use the default popups, use this.
+import "tui-date-picker/dist/tui-date-picker.css";
+import "tui-time-picker/dist/tui-time-picker.css";
+import "../ui/Calendar.css";
 
-const MyCalendar = (props) => {
-  const [events, setEvents] = useState([]);
-  const [mainModalIsOpen, setMainModalIsOpen] = useState(false);
-  const [attendeeModalIsOpen, setAttendeeModalIsOpen] = useState(false);
-  const [newEvent, setNewEvent] = useState({
-    id: null,
-    title: "",
-    content: "",
-    start: new Date(),
-    end: new Date(),
-    attendees: [],
-    createAt: new Date(),
-  });
-
-  useEffect(() => {
-    fetchEvents();
-  }, []);
-
-  // 이벤트 상태 업데이트
-  const fetchEvents = () => {
-    axios
-      .get("http://localhost:9000/api/v1/lighting_solutions/calendar/events")
-      .then((response) => {
-        setEvents(
-          response.data.map((event) => ({
-            id: event.calendarId,
-            title: event.calendarTitle,
-            content: event.calendarContent,
-            start: new Date(event.calendarStartAt),
-            end: new Date(event.calendarEndAt),
-            attendees: event.attendees || [],
-            createAt: new Date(event.calendarCreateAt),
-          }))
-        );
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the events!", error);
-      });
-  };
-
-  // 특정 세부 정보에 newEvent 상태 업데이트, 모달 열기
-  const fetchEventDetails = (eventId) => {
-    axios
-      .get(
-        `http://localhost:9000/api/v1/lighting_solutions/calendar/events/${eventId}`
-      )
-      .then((response) => {
-        const eventData = response.data;
-        setNewEvent({
-          id: eventData.calendarId,
-          title: eventData.calendarTitle,
-          content: eventData.calendarContent,
-          start: new Date(eventData.calendarStartAt),
-          end: new Date(eventData.calendarEndAt),
-          attendees: eventData.attendees || [],
-          createAt: new Date(eventData.calendarCreateAt),
-        });
-        setMainModalIsOpen(true);
-      })
-      .catch((error) => {
-        console.error(
-          `Error fetching event details for event ID ${eventId}`,
-          error
-        );
-      });
-  };
-
-  // 캘린더 슬롯 선택 시 newEvent 상태 업데이트 및 모달 열기
-  const handleSelectSlot = ({ start, end }) => {
-    setNewEvent({
-      id: null,
-      title: "",
-      content: "",
-      start,
-      end,
-      attendees: [],
-      createAt: new Date(),
-    });
-    setMainModalIsOpen(true);
-  };
-
-  // 캘린더 이벤트 선택 시 세부 정보 가져오기
-  const handleSelectEvent = (event) => {
-    fetchEventDetails(event.id);
-  };
-
-  // 새로운 이벤트 추가
-  const handleAddEvent = () => {
-    const startKST = moment(newEvent.start).tz("Asia/Seoul").toISOString();
-    const endKST = moment(newEvent.end).tz("Asia/Seoul").toISOString();
-    const attendees = newEvent.attendees || [];
-
-    axios
-      .post("http://localhost:9000/api/v1/lighting_solutions/calendar/events", {
-        calendarId: newEvent.id,
-        calendarTitle: newEvent.title,
-        calendarContent: newEvent.content,
-        calendarStartAt: startKST,
-        calendarEndAt: endKST,
-        calendarCreateAt: moment(newEvent.createAt)
-          .tz("Asia/Seoul")
-          .toISOString(),
-        attendees: attendees,
-      })
-      .then((response) => {
-        const newEventData = response.data;
-        setEvents([
-          ...events,
-          {
-            id: newEventData.calendarId,
-            title: newEventData.calendarTitle,
-            content: newEventData.calendarContent,
-            start: new Date(newEventData.calendarStartAt),
-            end: new Date(newEventData.calendarEndAt),
-            attendees: newEventData.attendees || [],
-            createAt: new Date(newEventData.calendarCreateAt),
-          },
-        ]);
-        // 참가자 추가 요청
-        addAttendeesToEvent(newEventData.calendarId, attendees);
-        setMainModalIsOpen(false);
-        clearNewEventState();
-      })
-      .catch((error) => {
-        console.error("There was an error adding the event!", error);
-      });
-  };
-
-  // 이벤트 업데이트
-  const handleUpdateEvent = () => {
-    const startKST = moment(newEvent.start).tz("Asia/Seoul").toISOString();
-    const endKST = moment(newEvent.end).tz("Asia/Seoul").toISOString();
-
-    axios
-      .put(
-        `http://localhost:9000/api/v1/lighting_solutions/calendar/events/${newEvent.id}`,
-        {
-          calendarTitle: newEvent.title,
-          calendarContent: newEvent.content,
-          calendarStartAt: startKST,
-          calendarEndAt: endKST,
-          attendees: newEvent.attendees,
-          calendarCreateAt: moment(newEvent.createAt)
-            .tz("Asia/Seoul")
-            .toISOString(),
-        }
-      )
-      .then((response) => {
-        const updatedEventData = response.data;
-        const updatedEvents = events.map((event) =>
-          event.id === updatedEventData.calendarId
-            ? {
-                id: updatedEventData.calendarId,
-                title: updatedEventData.calendarTitle,
-                content: updatedEventData.calendarContent,
-                start: new Date(updatedEventData.calendarStartAt),
-                end: new Date(updatedEventData.calendarEndAt),
-                attendees: updatedEventData.attendees || [],
-                createAt: new Date(updatedEventData.calendarCreateAt),
-              }
-            : event
-        );
-        setEvents(updatedEvents);
-        setMainModalIsOpen(false);
-        clearNewEventState();
-      })
-      .catch((error) => {
-        console.error("There was an error updating the event!", error);
-      });
-  };
-
-  // 이벤트 삭제
-  const handleDeleteEvent = () => {
-    axios
-      .delete(
-        `http://localhost:9000/api/v1/lighting_solutions/calendar/events/${newEvent.id}`
-      )
-      .then(() => {
-        const updatedEvents = events.filter(
-          (event) => event.id !== newEvent.id
-        );
-        setEvents(updatedEvents);
-        setMainModalIsOpen(false);
-        clearNewEventState();
-      })
-      .catch((error) => {
-        console.error("There was an error deleting the event!", error);
-      });
-  };
-
-  // 참가자 추가 요청
-  const addAttendeesToEvent = (eventId, attendees) => {
-    const attendeeRequests = attendees.map((attendee) =>
-      axios.post(
-        "http://localhost:9000/api/v1/lighting_solutions/calendar/attendees",
-        {
-          eventId: eventId,
-          team: attendee.team,
-          employee: attendee.employee,
-        }
-      )
-    );
-
-    Promise.all(attendeeRequests)
-      .then(() => {
-        fetchEvents(); // 참가자 추가 후 이벤트 목록 업데이트
-      })
-      .catch((error) => {
-        console.error("There was an error adding the attendees!", error);
-      });
-  };
-
-  // 참가자 추가
-  const handleAddAttendee = (team, employee) => {
-    const newAttendee = { team, employee };
-    setNewEvent((prevEvent) => ({
-      ...prevEvent,
-      attendees: [...prevEvent.attendees, newAttendee],
-    }));
-    setAttendeeModalIsOpen(false);
-  };
-
-  // 참가자 제거
-  const handleRemoveAttendee = (attendeeIndex) => {
-    const updatedAttendees = [...newEvent.attendees];
-    updatedAttendees.splice(attendeeIndex, 1);
-    setNewEvent((prevEvent) => ({
-      ...prevEvent,
-      attendees: updatedAttendees,
-    }));
-  };
-
-  // 초기화
-  const clearNewEventState = () => {
-    setNewEvent({
-      id: null,
-      title: "",
-      content: "",
-      start: new Date(),
-      end: new Date(),
-      attendees: [],
-      createAt: new Date(),
-    });
-  };
-
-  return (
-    <div>
-      <h4>일정 목록</h4>
-      <Calendar
-        localizer={localizer}
-        defaultDate={new Date()}
-        defaultView="month"
-        events={events}
-        selectable
-        onSelectSlot={handleSelectSlot}
-        onSelectEvent={handleSelectEvent}
-        style={{ height: "86vh", background: "white" }}
-      />
-      <Modal
-        isOpen={mainModalIsOpen}
-        onRequestClose={() => setMainModalIsOpen(false)}
-        contentLabel={newEvent.id ? "Edit Event" : "Add Event"}
-        className="custom-modal"
-        overlayClassName="custom-overlay"
-      >
-        <h2>{newEvent.id ? "Edit Event" : "Add Event"}</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            newEvent.id ? handleUpdateEvent() : handleAddEvent();
-          }}
-        >
-          <div className="form-group">
-            <label>Title</label>
-            <input
-              type="text"
-              className="form-control"
-              value={newEvent.title}
-              onChange={(e) =>
-                setNewEvent({ ...newEvent, title: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Content</label>
-            <input
-              type="text"
-              className="form-control"
-              value={newEvent.content}
-              onChange={(e) =>
-                setNewEvent({ ...newEvent, content: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Start Date</label>
-            <input
-              type="datetime-local"
-              className="form-control"
-              value={moment(newEvent.start).format("YYYY-MM-DDTHH:mm")}
-              onChange={(e) =>
-                setNewEvent({ ...newEvent, start: new Date(e.target.value) })
-              }
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>End Date</label>
-            <input
-              type="datetime-local"
-              className="form-control"
-              value={moment(newEvent.end).format("YYYY-MM-DDTHH:mm")}
-              onChange={(e) =>
-                setNewEvent({ ...newEvent, end: new Date(e.target.value) })
-              }
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Attendees</label>
-            {newEvent.attendees.map((attendee, index) => (
-              <div key={index} className="attendee-item">
-                <span>{`${attendee.team} - ${attendee.employee}`}</span>
-                <button
-                  type="button"
-                  className="btn btn-sm ml-2"
-                  onClick={() => handleRemoveAttendee(index)}
-                >
-                  <FaTimes />
-                </button>
-              </div>
-            ))}
-            <button
-              type="button"
-              className="btn btn-sm btn-primary mt-2"
-              onClick={() => setAttendeeModalIsOpen(true)}
-            >
-              Add Attendee
-            </button>
-          </div>
-          <div className="button-group">
-            <button type="submit" className="btn btn-primary">
-              {newEvent.id ? "Update Event" : "Add Event"}
-            </button>
-            {newEvent.id && (
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={handleDeleteEvent}
-              >
-                Delete Event
-              </button>
-            )}
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => setMainModalIsOpen(false)}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-      </Modal>
-      <Modal
-        isOpen={attendeeModalIsOpen}
-        onRequestClose={() => setAttendeeModalIsOpen(false)}
-        contentLabel="Add Attendee"
-        className="attendee-modal"
-        overlayClassName="custom-overlay"
-      >
-        <h2>Add Attendee</h2>
-        <AddAttendeeModal
-          isOpen={attendeeModalIsOpen}
-          closeModal={() => setAttendeeModalIsOpen(false)}
-          onAddAttendee={handleAddAttendee}
-          calendar={{
-            calendarId: newEvent.id,
-            calendarTitle: newEvent.title,
-            calendarCreateAt: newEvent.createAt,
-            calendarContent: newEvent.content,
-            calendarStartAt: newEvent.start,
-            calendarEndAt: newEvent.end,
-          }}
-        />
-        <button
-          type="button"
-          className="btn btn-secondary"
-          onClick={() => setAttendeeModalIsOpen(false)}
-        >
-          Close
-        </button>
-      </Modal>
-    </div>
-  );
+const myTheme = {
+  // Theme object to extends default dark theme.
 };
 
-export default MyCalendar;
+const today = new Date();
+
+const getDate = (type, date, value, operator) => {
+  const newDate = new Date(date);
+  if (type === "date") {
+    if (operator === "+") {
+      newDate.setDate(newDate.getDate() + value);
+    } else {
+      newDate.setDate(newDate.getDate() - value);
+    }
+  } else if (type === "hours") {
+    if (operator === "+") {
+      newDate.setHours(newDate.getHours() + value);
+    } else {
+      newDate.setHours(newDate.getHours() - value);
+    }
+  }
+  return newDate;
+};
+
+const CalendarComponent = () => {
+  useEffect(() => {
+    const calendar = new Calendar("#calendar", {
+      defaultView: "month",
+      taskView: true,
+      scheduleView: true,
+      useDetailPopup: true,
+      useCreationPopup: true,
+      template: {
+        monthDayname(dayname) {
+          return `<span class="calendar-week-dayname-name">${dayname.label}</span>`;
+        },
+        milestone(schedule) {
+          return `<span style="color:#fff;background-color: ${schedule.bgColor};">${schedule.title}</span>`;
+        },
+        milestoneTitle() {
+          return "Milestone";
+        },
+        allday(schedule) {
+          return `${schedule.title}<i class="fa fa-refresh"></i>`;
+        },
+        alldayTitle() {
+          return "All Day";
+        },
+      },
+      theme: myTheme,
+      week: {
+        showTimezoneCollapseButton: true,
+        timezonesCollapsed: true,
+      },
+      month: {
+        startDayOfWeek: 0,
+      },
+      timezones: [
+        {
+          timezoneOffset: 540,
+          displayLabel: "GMT+09:00",
+          tooltip: "Seoul",
+        },
+        {
+          timezoneOffset: -420,
+          displayLabel: "GMT-08:00",
+          tooltip: "Los Angeles",
+        },
+      ],
+    });
+
+    const calendars = [
+      {
+        id: "0",
+        name: "Private",
+        bgColor: "#9e5fff",
+        borderColor: "#9e5fff",
+      },
+      {
+        id: "1",
+        name: "Company",
+        bgColor: "#00a9ff",
+        borderColor: "#00a9ff",
+      },
+    ];
+
+    const schedules = [
+      {
+        id: "1",
+        calendarId: "0",
+        title: "TOAST UI Calendar Study",
+        category: "time",
+        dueDateClass: "",
+        start: today.toISOString(),
+        end: getDate("hours", today, 3, "+").toISOString(),
+      },
+      {
+        id: "2",
+        calendarId: "0",
+        title: "Practice",
+        category: "milestone",
+        dueDateClass: "",
+        start: getDate("date", today, 1, "+").toISOString(),
+        end: getDate("date", today, 1, "+").toISOString(),
+        isReadOnly: true,
+      },
+      {
+        id: "3",
+        calendarId: "0",
+        title: "FE Workshop",
+        category: "allday",
+        dueDateClass: "",
+        start: getDate("date", today, 2, "-").toISOString(),
+        end: getDate("date", today, 1, "-").toISOString(),
+        isReadOnly: true,
+      },
+      {
+        id: "4",
+        calendarId: "0",
+        title: "Report",
+        category: "time",
+        dueDateClass: "",
+        start: today.toISOString(),
+        end: getDate("hours", today, 1, "+").toISOString(),
+      },
+    ];
+
+    // 캘린더에 일정 추가
+    calendar.createSchedules(schedules);
+    // 캘린더 설정 추가
+    calendar.setCalendars(calendars);
+
+    // 일정 생성 이벤트 핸들러 추가
+    calendar.on("beforeCreateSchedule", (event) => {
+      const { start, end, title, calendarId } = event;
+      calendar.createSchedules([
+        {
+          id: String(Math.random()), // 임의의 ID 생성
+          calendarId: calendarId || "0", // 기본 캘린더 ID 설정
+          title,
+          category: "time",
+          dueDateClass: "",
+          start,
+          end,
+        },
+      ]);
+    });
+
+    // 일정 수정 이벤트 핸들러 추가
+    calendar.on("beforeUpdateSchedule", (event) => {
+      const { schedule, changes } = event;
+
+      calendar.updateSchedule(schedule.id, schedule.calendarId, changes);
+    });
+
+    // 일정 삭제 이벤트 핸들러 추가
+    calendar.on("beforeDeleteSchedule", (event) => {
+      const { id, calendarId } = event.schedule;
+
+      calendar.deleteSchedule(id, calendarId);
+    });
+
+    return () => {
+      calendar.destroy(); // 컴포넌트가 언마운트될 때 Calendar 인스턴스 해제
+    };
+  }, []); // 컴포넌트가 처음 마운트될 때 한 번만 실행
+
+  return <div id="calendar"></div>;
+};
+
+export default CalendarComponent;
