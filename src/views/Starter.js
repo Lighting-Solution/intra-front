0f6b29fdacd5b5d9c3aa7e06d7b77bdee5496948
kang <shkang19950@naver.com>
2024-07-10 17:36:38 +0900

    대시보드 필요없는 부분  삭제

--- src/views/Starter.js
+++ src/views/Starter.js
@@ -1,5 +1,4 @@
 import { Col, Row } from "reactstrap";
-import ProjectTables from "../components/dashboard/ProjectTable";
 import Menu from "../components/dashboard/Menu";
 import MyCalendar from "../components/Calendar";
 import DashboardNoticeBoard from "../components/dashboard/DashBoardNoticeBoard"; // DashboardNoticeBoard 컴포넌트 가져오기
@@ -10,12 +9,12 @@ const Starter = () => {
     <div>
       {/***Notice Board & Calendar ***/}
       <Row>
-        <Col sm="6" lg="8">
+        <Col sm="12" lg="8">
           <div className="dashboard-noticeboard">
             <DashboardNoticeBoard />
           </div>
         </Col>
-        <Col sm="6" lg="4">
+        <Col sm="12" lg="4">
           <div className="calendar-container">
             <MyCalendar />
           </div>
@@ -25,12 +24,6 @@ const Starter = () => {
       <Row>
         <Col lg="12">
           <Menu />
-        </Col>
-      </Row>
-      {/***Table ***/}
-      <Row>
-        <Col lg="12">
-          <ProjectTables />
         </Col>
       </Row>
     </div>
