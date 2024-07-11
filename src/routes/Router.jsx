7f8b8c20fd2c6d6920c8e2b07bbaf1f4dd50bc49
kang <shkang19950@naver.com>
2024-07-11 21:38:07 +0900

    게시판 리액트 수정
    ㅠㅠ

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,9 @@
-import { lazy } from "react";
+import { lazy, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
-
 /****Layouts*****/
 const FreeLayout = lazy(() => import("../views/ui/FreeLayout.jsx"));
 const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
 const ChatLayout = lazy(() => import("../views/ui/ChatLayout.jsx"));
-const NoticeLayout = lazy(() => import("../views/ui/NoticeLayout.jsx"));
 const Contact = lazy(() => import("../views/ui/Contact.js"));
 const DocumentLayout = lazy(() =>
   import("../views/ui/document/DocumentLayout.js")
@@ -65,80 +63,69 @@ const DocumentDetail = lazy(() =>
 );
 
 /*****Routes******/
-const ThemeRoutes = () => [
-  {
-    path: "/login",
-    exact: true,
-    element: <Login />,
-  },
-  {
-    path: "/",
-    element: <PrivateRoute element={<FullLayout />} />,
-    children: [
-      { path: "/", element: <Navigate to="/starter" /> },
-      { path: "/contact", exact: true, element: <Contact /> },
-      { path: "/starter", exact: true, element: <Starter /> },
-      { path: "/about", exact: true, element: <About /> },
-      { path: "/alerts", exact: true, element: <Alerts /> },
-      { path: "/badges", exact: true, element: <Badges /> },
-      { path: "/buttons", exact: true, element: <Buttons /> },
-      { path: "/cards", exact: true, element: <Cards /> },
-      { path: "/grid", exact: true, element: <Grid /> },
-      { path: "/table", exact: true, element: <Tables /> },
-      { path: "/forms", exact: true, element: <Forms /> },
-      { path: "/breadcrumbs", exact: true, element: <Breadcrumbs /> },
-      { path: "/calendar", exact: true, element: <Calendar /> },
-      { path: "/digitalapproval", exact: true, element: <DigitalApproval /> },
-      {
-        path: "/chat",
-        exact: true,
-        element: <ChatLayout />,
-      },
-      {
-        path: "/document",
-        element: <DocumentLayout />, // 새로운 DocumentLayout 사용
-        children: [
-          { path: "", exact: true, element: <DocumentComponent /> },
-          { path: "detail/:id", exact: true, element: <DocumentDetail /> },
-        ],
-      },
-      {
-        path: "notice",
-        element: <NoticeLayout />,
-        children: [
-          { path: "", exact: true, element: <NoticeBoard /> },
-          //  { path: "", exact: true, element: <NoticeBoardUser /> },
-          { path: "write", exact: true, element: <NoticeWriting /> },
-          { path: ":id", exact: true, element: <NoticeDetail /> },
-          // { path: ":id", exact: true, element: <NoticeDetailUser /> },
-          { path: "edit/:id", exact: true, element: <NoticeEditing /> },
-        ],
-      },
-      {
-        path: "freeboard",
-        element: <FreeLayout />,
-        children: [
-          { path: "", exact: true, element: <FreeBoard /> },
-          { path: "write", exact: true, element: <FreePostWriting /> },
-          { path: ":id", exact: true, element: <FreePostDetail /> },
-          { path: "edit/:id", exact: true, element: <FreePostEditing /> },
-        ],
-        path: "/digitalapproval/pending",
-        exact: true,
-        element: <PendingTable LoginEmpId={1} LoginPositionId={5} />,
-      },
-      {
-        path: "/digitalapproval/rejected",
-        exact: true,
-        element: <RejectedTable LoginEmpId={1} LoginPositionId={5} />,
-      },
-      {
-        path: "/freeboard",
-        exact: true,
-        element: <FreeBoard />,
-      },
-    ],
-  },
-];
+const ThemeRoutes = () => {
+  return [
+    {
+      path: "/login",
+      exact: true,
+      element: <Login />,
+    },
+    {
+      path: "/",
+      element: <PrivateRoute element={<FullLayout />} />,
+      children: [
+        { path: "/", element: <Navigate to="/starter" /> },
+        { path: "/contact", exact: true, element: <Contact /> },
+        { path: "/starter", exact: true, element: <Starter /> },
+        { path: "/about", exact: true, element: <About /> },
+        { path: "/alerts", exact: true, element: <Alerts /> },
+        { path: "/badges", exact: true, element: <Badges /> },
+        { path: "/buttons", exact: true, element: <Buttons /> },
+        { path: "/cards", exact: true, element: <Cards /> },
+        { path: "/grid", exact: true, element: <Grid /> },
+        { path: "/table", exact: true, element: <Tables /> },
+        { path: "/forms", exact: true, element: <Forms /> },
+        { path: "/breadcrumbs", exact: true, element: <Breadcrumbs /> },
+        { path: "/calendar", exact: true, element: <Calendar /> },
+        { path: "/digitalapproval", exact: true, element: <DigitalApproval /> },
+        {
+          path: "/chat",
+          exact: true,
+          element: <ChatLayout />,
+        },
+        {
+          path: "/document",
+          element: <DocumentLayout />, // 새로운 DocumentLayout 사용
+          children: [
+            { path: "", exact: true, element: <DocumentComponent /> },
+            { path: "detail/:id", exact: true, element: <DocumentDetail /> },
+          ],
+        },
+        { path: "/notice", exact: true, element: <NoticeBoard /> },
+        { path: "/notice/user", exact: true, element: <NoticeBoardUser /> },
+        {
+          path: "freeboard",
+          element: <FreeLayout />,
+          children: [
+            { path: "", exact: true, element: <FreeBoard /> },
+            { path: "write", exact: true, element: <FreePostWriting /> },
+            { path: ":id", exact: true, element: <FreePostDetail /> },
+            { path: "edit/:id", exact: true, element: <FreePostEditing /> },
+          ],
+        },
+        {
+          path: "/digitalapproval/pending",
+          exact: true,
+          element: <PendingTable LoginEmpId={1} LoginPositionId={5} />,
+        },
+        {
+          path: "/digitalapproval/rejected",
+          exact: true,
+          element: <RejectedTable LoginEmpId={1} LoginPositionId={5} />,
+        },
+      ],
+    },
+  ];
+};
 
 export default ThemeRoutes;
